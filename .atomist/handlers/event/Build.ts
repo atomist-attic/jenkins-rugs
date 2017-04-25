5b136ba12d85f52de2d81a86f3bfb532aa261122
David Dooling <david@atomist.com>
2017-04-24 21:26:37 -0500

    Update build and TypeScript support files
    Use UpdateSupportFiles editor, removing files it neglected to.
    
    Update README.  Delint.  Increment version.

--- .atomist/handlers/event/Build.ts
+++ .atomist/handlers/event/Build.ts
@@ -1,8 +1,26 @@
-import { EventPlan, HandleEvent, LifecycleMessage, DirectedMessage, UserAddress } from '@atomist/rug/operations/Handlers'
-import { GraphNode, Match, PathExpression } from '@atomist/rug/tree/PathExpression'
-import { EventHandler, Tags } from '@atomist/rug/operations/Decorators'
+/*
+ * Copyright © 2017 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import { Build } from '@atomist/cortex/Build'
+import { EventHandler, Tags } from "@atomist/rug/operations/Decorators";
+import {
+    DirectedMessage, EventPlan, HandleEvent, LifecycleMessage, UserAddress,
+} from "@atomist/rug/operations/Handlers";
+import { GraphNode, Match, PathExpression } from "@atomist/rug/tree/PathExpression";
+
+import { Build } from "@atomist/cortex/Build";
 
 @EventHandler("JenkinsBuilds", "Handle build events from Jenkins",
     new PathExpression<Build, Build>(
@@ -17,30 +35,30 @@ import { Build } from '@atomist/cortex/Build'
                 [/repo::Repo()]]`))
 @Tags("ci", "jenkins")
 class Built implements HandleEvent<Build, Build> {
-    handle(event: Match<Build, Build>): EventPlan {
-        let build = event.root()
-        let plan = new EventPlan()
+    public handle(event: Match<Build, Build>): EventPlan {
+        const build = event.root();
+        const plan = new EventPlan();
 
-        let repo = build.repo.name
-        let owner = build.repo.owner
-        let cid = "commit_event/" + owner + "/" + repo + "/" + build.commit.sha
+        const repo = build.repo.name;
+        const owner = build.repo.owner;
+        const cid = "commit_event/" + owner + "/" + repo + "/" + build.commit.sha;
 
-        let message = new LifecycleMessage(build, cid)
+        const message = new LifecycleMessage(build, cid);
 
-        if (build.status == "failed" || build.status == "broken") {
+        if (build.status === "failed" || build.status === "broken") {
             try {
                 if (build.commit.author != null && build.commit.author.person != null) {
-                    let body = `Jenkins build \`#${build.name}\` of \`${owner}/${repo}\` failed after your last commit \`${build.commit.sha}\`: ${build.buildUrl}`
-                    let address = new UserAddress(build.commit.author.person.chatId.id)
-                    plan.add(new DirectedMessage(body, address))
+                    const body = `Jenkins build \`#${build.name}\` of \`${owner}/${repo}\` failed` +
+                        ` after your last commit \`${build.commit.sha}\`: ${build.buildUrl}`;
+                    const address = new UserAddress(build.commit.author.person.chatId.id);
+                    plan.add(new DirectedMessage(body, address));
                 }
-            }
-            catch (e) {
-                console.log((<Error>e).message)
+            } catch (e) {
+                console.log((e as Error).message);
             }
         }
-        plan.add(message)
-        return plan
+        plan.add(message);
+        return plan;
     }
 }
-export const built = new Built()
+export const built = new Built();
--- README.md
+++ README.md
@@ -19,6 +19,47 @@ If you find a problem, please create an [issue][].
 
 [issue]: https://github.com/atomist/jenkins-handlers/issues
 
+## Contributing
+
+If you are interested in contributing to the Atomist open source
+projects, please see our [contributing guidelines][contrib] and
+our [code of conduct][code].
+
+[contrib]: https://github.com/atomist/welcome/blob/master/CONTRIBUTING.md
+[code]: https://github.com/atomist/welcome/blob/master/CODE_OF_CONDUCT.md
+
+## Developing
+
+You can build, test, and install the project locally with
+the [Rug CLI][cli].
+
+[cli]: https://github.com/atomist/rug-cli
+
+```
+$ rug test
+$ rug install
+```
+
+To clean up cached files and update TypeScript dependencies, run this
+command.
+
+```
+$ ( cd .atomist && find editors generators handlers tests -name '*.js' -print0 | xargs -0 rm; rm -rf node_modules; yarn && rug clean )
+```
+
+To create a new release of the project, simply push a tag of the form
+`M.N.P` where `M`, `N`, and `P` are integers that form the next
+appropriate [semantic version][semver] for release.  For example:
+
+[semver]: http://semver.org
+
+```
+$ git tag -a 1.2.3
+```
+
+The Travis CI build (see badge at the top of this page) will upload
+the needed artifacts.
+
 ---
 Created by [Atomist][atomist].
 Need Help?  [Join our Slack team][slack].
